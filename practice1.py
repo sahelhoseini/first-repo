362071f7b2cc6a65da2a1238a851b4ef1c279d1a
sahelhoseini <sahellhoseini99@yahoo.com>
2022-11-08 16:52:31 +0330

    uplode practice1.py

---
+++ practice1.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+string=str(input("inter your string:"))
+integer=int(input("inter your integer:"))
+i=0
+while i<integer:
+ print(string[i])
+ i+=1
+   
